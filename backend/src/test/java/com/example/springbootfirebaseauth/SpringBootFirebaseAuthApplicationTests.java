213ff84ac33d4135cce3082669b22f40013127a5
dimas-maxxitani <dimas@maxxitani.id>
2021-08-05 22:49:05 +0700

    Add backend & frontend init app

---
+++ backend/src/test/java/com/example/springbootfirebaseauth/SpringBootFirebaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootfirebaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirebaseAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
