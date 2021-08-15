84ef192791f6a4a53189fc60ef9b5f7c0904cbbb
dimaspriyo <dimspriyo@gmail.com>
2021-08-15 23:00:22 +0700

    Add createUser method

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
+import {FirebaseService} from "../service/firebase/firebase.service";
 
 @Component({
   selector: 'app-login-component',
@@ -16,6 +17,11 @@ export class LoginComponent implements OnInit {
     password: ['']
   })
 
+  constructor(
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService
+  ) {
+  }
 
   get getEmail() {
     return this.appForm.get('email');
@@ -25,17 +31,16 @@ export class LoginComponent implements OnInit {
     return this.appForm.get('password');
   }
 
-  constructor(
-    private fb: FormBuilder
-  ) {
-  }
+
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+    this.firebaseService.getAllUsers();
     if (this.formTitle == "Login") {
-      console.log("Do some Login logic");
+      console.log("Do some Login logic ");
+    this.firebaseService.createUser(this.appForm.value);
     } else if (this.formTitle == "Register") {
       console.log("Do some Register logic");
     }
--- frontend/src/app/service/firebase/firebase.service.ts
+++ frontend/src/app/service/firebase/firebase.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {User} from "../../model/User";
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,8 @@ import {AngularFireAuth} from "@angular/fire/auth";
 export class FirebaseService {
 
   constructor(
-    public angularFireAuth : AngularFireAuth
+    private angularFireAuth : AngularFireAuth,
+    private angularFireStore : AngularFirestore
   ) {}
 
   async signIn(email : string, password : string){
@@ -21,5 +24,25 @@ export class FirebaseService {
   async signOut(){
     await this.angularFireAuth.signOut();
     localStorage.removeItem('user');
+  }
+
+   createUser(user: User){
+     this.angularFireStore
+      .collection("firebase-authentication")
+      .add(user)
+       .then(res => console.log(res),err => console.log(err));
+  }
+
+  getAllUsers(){
+    let users = this.angularFireStore
+      .collection("firebase-authentication")
+      .snapshotChanges();
+
+    console.log(users);
+    users.subscribe(res => {
+      res.forEach(value => console.log(value.payload.doc.data()));
+    });
+
+
   }
 }
